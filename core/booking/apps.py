9ab53cb91492f9c70abfb214e5dc91ce3171c9d5
oldcorvus <moelcrow@gmail.com>
2025-02-14 23:54:07 +0100

    chore: Configure Redis cache and PostgreSQL database

--- core/booking/apps.py
+++ core/booking/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BookingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "booking"
+
+    def ready(self):
+        from .domain.models.reservation import Reservation
+        from .domain.models.table import Table
+
