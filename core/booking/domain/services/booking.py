6a129f4723dbdece9c1c40890e3dfd9453fa9fad
oldcorvus <moelcrow@gmail.com>
2025-02-15 00:01:22 +0100

    feat:  Implement BookingService with concurrency strategies

---
+++ core/booking/domain/services/booking.py
@@ -0,0 +1,46 @@
+from django.db import transaction
+from django.db.models import F
+from ..models.seat import SeatCount
+from ..models.table import Table
+from ..models.reservation import Reservation, ReservationStatus
+from .pricing.base import BasePricingStrategy
+from .concurrency.base import ConcurrencyStrategy
+from .concurrency.high import HighConcurrencyStrategy
+from .concurrency.normal import NormalConcurrencyStrategy
+
+class BookingService:
+  
+    LAST_TABLES_THRESHOLD = 5
+
+    def __init__(self):
+        self.strategies = {
+            "normal": NormalConcurrencyStrategy(),
+            "high": HighConcurrencyStrategy()
+        }
+
+    def create_booking(
+        self, 
+        user, 
+        num_individuals: SeatCount, 
+        pricing_strategy: BasePricingStrategy
+    ) -> Reservation:
+        """Selects and executes the appropriate concurrency strategy"""
+        strategy = self._select_concurrency_strategy()
+        return strategy.create_reservation(
+            self, 
+            user,
+            num_individuals,
+            pricing_strategy
+        )
+
+    def _select_concurrency_strategy(self) -> ConcurrencyStrategy:
+        if self._should_use_redis():
+            return self.strategies["high"]
+        return self.strategies["normal"]
+
+    def _should_use_redis(self) -> bool:
+        return Table.objects.filter(is_booked=False).count() <= self.LAST_TABLES_THRESHOLD
+
+
+
+
