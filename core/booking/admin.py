6caf68f998610728fe632330827b12ac2a57b8c8
oldcorvus <moelcrow@gmail.com>
2025-02-14 23:42:01 +0100

    feat: Register Reservation and Table models in Django admin

--- core/booking/admin.py
+++ core/booking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .domain.models.reservation import Reservation
+from .domain.models.table import Table
 # Register your models here.
+admin.site.register(Reservation)
+admin.site.register(Table)
