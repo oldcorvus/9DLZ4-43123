576a3cf42c3417d61eb91150be81c17adc881791
oldcorvus <moelcrow@gmail.com>
2025-02-14 23:37:11 +0100

    feat: Add Reservation model as aggregate root

--- core/booking/domain/models/reservation.py
+++ core/booking/domain/models/reservation.py
@@ -11,3 +11,29 @@ class ReservationStatus(models.TextChoices):
     PENDING = 'PND', 'Pending'
     CONFIRMED = 'CFM', 'Confirmed'
     CANCELLED = 'CNL', 'Cancelled'
+
+class Reservation(models.Model):
+    """Aggregate Root for Reservation management"""
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='reservations'
+    )
+    table = models.ForeignKey(
+        'Table',
+        on_delete=models.PROTECT,
+        related_name='reservations'
+    )
+    start_time = models.DateTimeField(null=True,blank=True)
+    end_time = models.DateTimeField(null=True,blank=True)
+    cost_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    cost_currency = models.CharField(max_length=3, default='USD')
+    status = models.CharField(
+        max_length=3,
+        choices=ReservationStatus.choices,
+        default=ReservationStatus.PENDING
+    )
+    version = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
